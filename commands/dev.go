1b94fe4b68321f554c8e5f29d70d7de6883a0cf8
vtgare <tatarinvlad1@gmail.com>
2020-08-05 13:53:27 +0300

    message command

--- commands/dev.go
+++ commands/dev.go
@@ -3,6 +3,7 @@ package commands
 import (
 	"context"
 	"strconv"
+	"strings"
 
 	"github.com/VTGare/boe-tea-go/database"
 	"github.com/VTGare/boe-tea-go/utils"
@@ -15,6 +16,28 @@ func init() {
 	dg.IsVisible = false
 	dg.AddCommand("migrate", migrateDB)
 	dg.AddCommand("test", test)
+	dg.AddCommand("message", message)
+}
+
+func message(s *discordgo.Session, m *discordgo.MessageCreate, args []string) error {
+	if m.Author.ID != utils.AuthorID {
+		return nil
+	}
+
+	if len(args) == 0 {
+		return nil
+	}
+
+	for _, g := range s.State.Guilds {
+		for _, ch := range g.Channels {
+			if strings.Contains(ch.Name, "general") && ch.Type == discordgo.ChannelTypeGuildText {
+				s.ChannelMessageSend(ch.ID, strings.Join(args, " "))
+				break
+			}
+		}
+	}
+
+	return nil
 }
 
 func migrateDB(s *discordgo.Session, m *discordgo.MessageCreate, args []string) error {
