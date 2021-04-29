aa4df8d7dfadca05b34263edb0a7c72d462d4888
vtgare <tatarinvlad1@gmail.com>
2021-04-29 20:10:39 +0300

    Crosspost groups pushing/removing bugfix

--- pkg/models/users/users.go
+++ pkg/models/users/users.go
@@ -258,7 +258,7 @@ func (u userService) DeleteGroup(ctx context.Context, userID, group string) (*Us
 func (u userService) InsertToGroup(ctx context.Context, userID, group, child string) (*User, error) {
 	res := u.col().FindOneAndUpdate(
 		ctx,
-		bson.M{"user_id": userID, "channel_groups.name": group, "channel_groups.children": bson.M{"$nin": []string{child}}},
+		bson.M{"user_id": userID, "channel_groups.name": group},
 		bson.M{"$addToSet": bson.M{"channel_groups.$.children": child}},
 		options.FindOneAndUpdate().SetReturnDocument(options.After),
 	)
@@ -275,7 +275,7 @@ func (u userService) InsertToGroup(ctx context.Context, userID, group, child str
 func (u userService) DeleteFromGroup(ctx context.Context, userID, group, child string) (*User, error) {
 	res := u.col().FindOneAndUpdate(
 		ctx,
-		bson.M{"user_id": userID, "channel_groups.name": group, "channel_groups.children": bson.M{"$in": []string{child}}},
+		bson.M{"user_id": userID, "channel_groups.name": group},
 		bson.M{"$pull": bson.M{"channel_groups.$.children": child}},
 		options.FindOneAndUpdate().SetReturnDocument(options.After),
 	)
@@ -328,6 +328,16 @@ func (u userService) ReplaceOne(ctx context.Context, user *User) (*User, error) 
 func (u *User) FindGroup(parentID string) (*Group, bool) {
 	for _, group := range u.Groups {
 		if group.Parent == parentID {
+			return group, true
+		}
+	}
+
+	return nil, false
+}
+
+func (u *User) FindGroupByName(name string) (*Group, bool) {
+	for _, group := range u.Groups {
+		if group.Name == name {
 			return group, true
 		}
 	}
